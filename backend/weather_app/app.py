376786cb47669d3b50e9a6c1a50c68b0d235642e
A.Bond <alexey.bond.94.55@gmail.com>
2015-03-12 15:39:41 +0600

    Add administration page & API calls for administration and debugging.

--- backend/weather_app/app.py
+++ backend/weather_app/app.py
@@ -1,5 +1,7 @@
 from flask import *
 import pymongo
+import bson
+import json
 
 import yawparser
 
@@ -16,6 +18,14 @@ db = db_client[DB_NAME]
 
 app = Flask(__name__)
 
+###
+
+def request_wants_json():
+	best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
+	return best == 'application/json' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+### Error handlers
+
 @app.errorhandler(500)
 def page_not_found(e):
 	print "\n\n",traceback.format_exc(),"\n\n"
@@ -25,6 +35,8 @@ def page_not_found(e):
 def weather1():
 	city_id = request.args.get('city_id',None)
 	return weather(city_id=city_id)
+
+### Main part of API
 
 @app.route('/api/v0/weather/<int:city_id>',methods=['GET'])
 def weather(city_id=None):
@@ -129,6 +141,60 @@ def cloth_choose():
 	print '>>>> ',str(choosen)
 
 	return jsonify({'choise':choosen})
+
+### API Calls for debugging/administration
+
+@app.route('/api/v0/cloth/item/<int:item_id>',methods=['POST'])
+def cloth_postitem(item_id=0):
+	postitem = {'description':{}}
+	postitem['description']['name'] = request.form['description.name']
+	postitem['description']['description'] = request.form['description.description']
+	postitem['description']['group'] = request.form['description.group']
+	postitem['description']['img'] = request.form['description.img']
+	postitem['id'] = int(request.form['id'])
+	postitem['_id'] = bson.ObjectId(request.form['_id'])
+	postitem['conditions'] = json.loads(request.form['conditions'])
+	db.cloth_items.save(postitem)
+	if request_wants_json():
+		return jsonify({'status':'OK'})
+	else:
+		return redirect('/frontend/admin.html')
+
+@app.route('/api/v0/weather/<int:city_id>',methods=['POST'])
+def weathercache_post(city_id=0):
+	postrecord={'state':{}}
+	postrecord['city_id'] = int(request.form['id'])
+	postrecord['_id'] = bson.ObjectId(request.form['_id'])
+	postrecord['updated'] = datetime.strptime(request.form['updated'],"%a, %d %b %Y %H:%M:%S +0000")
+	postrecord['state']['temperature'] = float(request.form['state.temperature'])
+	postrecord['state']['windVelocity'] = float(request.form['state.windVelocity'])
+	postrecord['state']['windDirection'] = request.form['state.windDirection']
+	postrecord['state']['weatherInWords'] = request.form['state.weatherInWords']
+	postrecord['state']['humidity'] = float(request.form['state.humidity'])
+	postrecord['state']['weatherThumbnailURL'] = request.form['state.weatherThumbnailURL']
+	db.weather_cache.save(postrecord)
+	if request_wants_json():
+		return jsonify({'status':'OK'})
+	else:
+		return redirect('/frontend/admin.html')
+
+
+@app.route('/api/v0/cloth/items',methods=['GET'])
+def cloth_getitems():
+	items = list(db.cloth_items.find())
+	for item in items:
+		item['_id'] = str(item['_id'])
+	return jsonify({'items':items})
+
+@app.route('/api/v0/weather/cached',methods=['GET'])
+def weathercache_get():
+	records = list(db.weather_cache.find())
+	for rec in records:
+		rec['_id'] = str(rec['_id'])
+		rec['updated'] = rec['updated'].strftime("%a, %d %b %Y %H:%M:%S +0000")
+	return jsonify({'records':records})
+
+
 
 if __name__ == '__main__':
 	app.run( )
