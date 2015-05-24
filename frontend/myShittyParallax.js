3b20f9ff6149ccb0d71c257d3b8983563f98b4dd
moardm <moiseyenkoa@gmail.com>
2015-05-25 04:15:02 +0600

    backround selected by timeZ

--- frontend/myShittyParallax.js
+++ frontend/myShittyParallax.js
@@ -1,14 +1,30 @@
 function wind(obj, speed){
   var start = Date.now();
-  var fps = 50;
   var timer = setInterval(function() {
     var timePassed = Date.now() - start;
     draw(obj,timePassed*speed);
-  }, 20);
+  }, 50);
   obj.css({backgroundPosition: '200px 0px'});
 }
 
 
 function draw(obj,timePassed) {
   obj.css({backgroundPosition: timePassed+'px 0px'});
+}
+
+function season(currentdate){
+  var mnth = Math.floor((currentdate.getMonth()-2)/3);
+  if (mnth<0){
+    return 3;
+  }else{
+    return mnth;
+  }
+}
+
+function isNight(currentdate){
+  if ((currentdate.getHours()>6) && (currentdate.getHours()<22)){
+    return false;
+  }else{
+    return true;
+  };
 }
